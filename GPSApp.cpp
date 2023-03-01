11f47c3436ae982ead39bc0a5cc93ce0d04df629
Robin Pilapil <robin.pilapil@gmail.com>
2023-03-01 10:52:29 -0800

    did some of the user input stuff

--- GPSApp.cpp
+++ GPSApp.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
+// make main method and user input stuff
+// create the method headers
+//
 
 
 int main(){
+    bool isInUse = true; 
 
+    while(isInUse){
+        int userInput; 
+        cout << "Choose an option: " << "\n1)Store a new location" 
+        << "\n2)Remove a location" << "\n3)Show stored location" 
+        << "\n4)Exit" << endl; 
+        cin >> userInput; 
+        switch (userInput)
+        {
+        case 1:
+            break;
+        case 2: 
+            break;
+        case 3: 
+            break; 
+        case 4: 
+            isInUse = false; 
+            break; 
+        default:
+            break;
+        }
+    }
+    
 }
