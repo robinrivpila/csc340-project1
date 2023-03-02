bed7d3eeb07a43181e78aa74b8e9044f2782878e
Robin Pilapil <robin.pilapil@gmail.com>
2023-03-02 10:15:15 -0800

    got adding locations and showing locations down

--- GPSApp.cpp
+++ GPSApp.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 using namespace std;
 
-// make main method and user input stuff
-// create the method headers
-//
 int storeNewLocation(int *, int *, int);
+int removeLocation(int*, int* , int); 
+void showStoredLocations(int*, int*, int); 
+
+
+void printArray(int*, int); 
 
 int main()
 {
-bool isInUse = true;
+    bool isInUse = true;
+    int numOfElements = 0;
+    const int capacity = 10;
+    int *longitude = new int[capacity];
+    int *latitude = new int[capacity];
+    for (int i = 0; i < capacity; i++)
+    {
+        longitude[i] = 0;
+        latitude[i] = 0;
+    }
 
     while (isInUse)
     {
-
-        
-        const int capacity = 10;
-        int *longitude = new int[capacity];
-        int *latitude = new int[capacity];
-        int numOfElements = 0;
-
-        for (int i = 0; i < capacity; i++)
-        {
-            longitude[i] = 0;
-            latitude[i] = 0;
-        }
-
         int userInput;
         cout << "Choose an option: "
              << "\n1)Store a new location"
@@ -35,13 +33,13 @@ bool isInUse = true;
         switch (userInput)
         {
         case 1:
-            //trying to get numOfElements to actually update rn
-            numOfElements = storeNewLocation(longitude, latitude,numOfElements);
-            
+            numOfElements = storeNewLocation(longitude, latitude,numOfElements);  
             break;
         case 2:
+            numOfElements = removeLocation(longitude, latitude, numOfElements); 
             break;
         case 3:
+            showStoredLocations(longitude, latitude, numOfElements); 
             break;
         case 4:
             isInUse = false;
@@ -58,9 +56,37 @@ int storeNewLocation(int *xValues, int *yValues, int numOfElements)
     int x, y;
     cout << "Enter X coordinate: ";
     cin >> x;
+    xValues[numOfElements] = x; 
     cout << "Enter Y coordinate: ";
     cin >> y;
-    cout << "X coordinate: " << x << endl;
-    cout << "Y coordinate: " << y << endl;
-    return numOfElements++; 
+    yValues[numOfElements] = y; 
+    cout << "Location " << x << ", " << y << " has been stored!" << endl; 
+    return numOfElements+=1; 
 }
+
+int removeLocation(int* xValues, int* yValues, int numOfElements){
+    int userInput; 
+    cout << "The following locations are stored: " << endl; 
+    showStoredLocations(xValues, yValues, numOfElements); 
+    cout << "Which location would you like to remove? " << endl; 
+    numOfElements--;
+    cin >> userInput; 
+    //THIS IS NOT WORKING PROPERLY
+    for(int i = userInput; i < numOfElements; i++){
+        xValues[userInput-1]= xValues[userInput];  
+        yValues[userInput-1]= yValues[userInput]; 
+    }
+    return numOfElements; 
+}
+
+void showStoredLocations(int* xValues, int* yValues, int numOfElements){
+    for(int i = 0; i< numOfElements; i++){
+        cout << i+1 << ": " << xValues[i] << ", " << yValues[i] << endl; 
+    }
+}
+
+
+
+
+
+
