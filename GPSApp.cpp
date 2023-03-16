12d958b3d81a0e45a6f70956371ddfdca904ea72
Robin Pilapil <robin.pilapil@gmail.com>
2023-03-16 13:05:56 -0700

    added error checking and invalid msgs

--- GPSApp.cpp
+++ GPSApp.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int storeNewLocation(int *, int *, int);
-int removeLocation(int*, int* , int); 
+int storeNewLocation(int *, int *, int, int);
+int removeLocation(int*, int* , int, int); 
 void showStoredLocations(int*, int*, int); 
 
-
-void printArray(int*, int); 
-
-int main()
-{
+int main(){
     bool isInUse = true;
     int numOfElements = 0;
     const int capacity = 10;
@@ -30,13 +26,12 @@ int main()
              << "\n3)Show stored location"
              << "\n4)Exit" << endl;
         cin >> userInput;
-        switch (userInput)
-        {
+        switch (userInput){
         case 1:
-            numOfElements = storeNewLocation(longitude, latitude,numOfElements);  
+            numOfElements = storeNewLocation(longitude, latitude,numOfElements,capacity);  
             break;
         case 2:
-            numOfElements = removeLocation(longitude, latitude, numOfElements); 
+            numOfElements = removeLocation(longitude, latitude, numOfElements, capacity); 
             break;
         case 3:
             showStoredLocations(longitude, latitude, numOfElements); 
@@ -50,29 +45,34 @@ int main()
     }
 }
 
-int storeNewLocation(int *xValues, int *yValues, int numOfElements)
-{
-    int x, y;
-    cout << "Enter X coordinate: ";
-    cin >> x;
-    xValues[numOfElements] = x; 
-    cout << "Enter Y coordinate: ";
-    cin >> y;
-    yValues[numOfElements] = y; 
-    cout << "Location " << x << ", " << y << " has been stored!" << endl; 
-    return numOfElements+=1; 
+int storeNewLocation(int *xValues, int *yValues, int numOfElements, int capacity){
+    if(numOfElements < capacity){
+        int x, y;
+        cout << "Enter X coordinate: ";
+        cin >> x;
+        xValues[numOfElements] = x; 
+        cout << "Enter Y coordinate: ";
+        cin >> y;
+        yValues[numOfElements] = y; 
+        cout << "Location " << x << ", " << y << " has been stored!" << endl; 
+        return numOfElements+=1; 
+    }else{
+        cout << "Location List Capacity is full!" << endl; 
+        return numOfElements;
+    }
+    
 }
 
-int removeLocation(int* xValues, int* yValues, int numOfElements){
+int removeLocation(int* xValues, int* yValues, int numOfElements, int capacity){
     int userInput; 
     cout << "The following locations are stored: " << endl; 
     showStoredLocations(xValues, yValues, numOfElements); 
     cout << "Which location would you like to remove? " << endl; 
-    
     cin >> userInput; 
+    if(userInput <= capacity && userInput > 0 ){
     int x = xValues[userInput-1];
     int y = yValues[userInput-1]; 
-    //I THINNK IT WORKS NOW
+
     if(userInput == numOfElements){
         xValues[userInput+1]=0;
         yValues[userInput+1]=0;
@@ -84,6 +84,9 @@ int removeLocation(int* xValues, int* yValues, int numOfElements){
     }
     numOfElements--;
     cout << "Location " << x << ", " << y << " has been removed from memory!" << endl; 
+    }else{
+        cout << "Invalid user input!" << endl; 
+    }
     return numOfElements; 
 }
 
